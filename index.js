404752d45cc9539ace88e06361fd1751d3141c87
jbjzeehad <ilamta.97@gmail.com>
2023-12-25 02:45:40 +0600

    add data into db

--- index.js
+++ index.js
@@ -30,12 +30,34 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const database = client.db('libraryDB');
+
+        const bookCollection = database.collection('allbooks');
+
+        app.post('/allbooks', async (req, res) => {
+            const addbooks = req.body;
+            console.log('New book:', addbooks);
+            const addingBooks = await bookCollection.insertOne(addbooks);
+            res.send(addingBooks);
+        })
+
+
+
+
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
